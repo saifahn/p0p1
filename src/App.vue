b6e99394c6ea308cd69802e7d4b7a01c3884476c
Sean Li <9913851+saifahn@users.noreply.github.com>
2026-04-17 20:40:06 +0900

    refactor: map over cards

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed } from 'vue'
+import { computed, reactive, type ComputedRef, type Ref } from 'vue'
 
 import cardData from './sets/SOS-slimmed.json'
 import { ref } from 'vue'
@@ -9,66 +9,60 @@ const cards = ref(cardData)
 
 const commonCards = computed(() => cards.value.filter((card) => card.rarity === 'common'))
 
-const selectedWhiteCard = ref<Card>()
-const whiteCommons = computed(() =>
-  commonCards.value.filter((card) => card.colors.length === 1 && card.colors[0] === 'W'),
-)
+type MapOfCards = {
+  [key: string]: {
+    label: string
+    selected: Ref<Card | undefined>
+    cards: ComputedRef<Card[]>
+  }
+}
 
-const selectedBlueCard = ref<Card>()
-const blueCommons = computed(() =>
-  commonCards.value.filter((card) => card.colors.length === 1 && card.colors[0] === 'U'),
-)
+type MtgColorCode = 'W' | 'U' | 'B' | 'R' | 'G'
 
-const selectedBlackCard = ref<Card>()
-const blackCommons = computed(() =>
-  commonCards.value.filter((card) => card.colors.length === 1 && card.colors[0] === 'B'),
-)
+function isCardExactlyColor(card: Card, color: MtgColorCode) {
+  return card.colors.length === 1 && card.colors[0] == color
+}
 
-const selectedRedCard = ref<Card>()
-const redCommons = computed(() =>
-  commonCards.value.filter((card) => card.colors.length === 1 && card.colors[0] === 'R'),
-)
-
-const selectedGreenCard = ref<Card>()
-const greenCommons = computed(() =>
-  commonCards.value.filter((card) => card.colors.length === 1 && card.colors[0] === 'G'),
-)
+const mapOfCards = reactive({
+  whiteCommons: {
+    label: 'White Common',
+    selected: ref(),
+    cards: computed(() => commonCards.value.filter((card) => isCardExactlyColor(card, 'W'))),
+  },
+  blueCommons: {
+    label: 'Blue Common',
+    selected: ref(),
+    cards: computed(() => commonCards.value.filter((card) => isCardExactlyColor(card, 'U'))),
+  },
+  blackCommons: {
+    label: 'Black Common',
+    selected: ref(),
+    cards: computed(() => commonCards.value.filter((card) => isCardExactlyColor(card, 'B'))),
+  },
+  redCommons: {
+    label: 'Red Common',
+    selected: ref(),
+    cards: computed(() => commonCards.value.filter((card) => isCardExactlyColor(card, 'R'))),
+  },
+  greenCommons: {
+    label: 'Green Common',
+    selected: ref(),
+    cards: computed(() => commonCards.value.filter((card) => isCardExactlyColor(card, 'G'))),
+  },
+} satisfies MapOfCards)
 </script>
 
 <template>
   <UApp>
     <UContainer as="main" class="p-2">
-      <h1>SOS p0p1</h1>
       <form>
         <CardChoice
-          :cards="whiteCommons"
-          :selected-card="selectedWhiteCard"
-          label="White Common"
-          @select-card="(card) => (selectedWhiteCard = card)"
-        />
-        <CardChoice
-          :cards="blueCommons"
-          :selected-card="selectedBlueCard"
-          label="Blue Common"
-          @select-card="(card) => (selectedBlueCard = card)"
-        />
-        <CardChoice
-          :cards="blackCommons"
-          :selected-card="selectedBlackCard"
-          label="Black Common"
-          @select-card="(card) => (selectedBlackCard = card)"
-        />
-        <CardChoice
-          :cards="redCommons"
-          :selected-card="selectedRedCard"
-          label="Red Common"
-          @select-card="(card) => (selectedRedCard = card)"
-        />
-        <CardChoice
-          :cards="greenCommons"
-          :selected-card="selectedGreenCard"
-          label="Green Common"
-          @select-card="(card) => (selectedGreenCard = card)"
+          v-for="category in Object.values(mapOfCards)"
+          :key="category.label"
+          :label="category.label"
+          :cards="category.cards"
+          :selected-card="category.selected"
+          @select-card="(card) => (category.selected = card)"
         />
       </form>
     </UContainer>
