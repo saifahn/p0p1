87fd92b130fcc1f5feed6a42c234562b7dd6b641
Sean Li <9913851+saifahn@users.noreply.github.com>
2026-04-17 10:25:08 +0900

    feat: add all the common cards

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,31 @@ import CardChoice, { type Card } from './components/CardChoice.vue'
 
 const cards = ref(cardData)
 
-const selectedWhiteCard = ref<Card>()
+const commonCards = computed(() => cards.value.filter((card) => card.rarity === 'common'))
 
-const whiteCards = computed(() =>
-  cards.value.filter((card) => card.colors.length === 1 && card.colors[0] === 'W'),
+const selectedWhiteCard = ref<Card>()
+const whiteCommons = computed(() =>
+  commonCards.value.filter((card) => card.colors.length === 1 && card.colors[0] === 'W'),
+)
+
+const selectedBlueCard = ref<Card>()
+const blueCommons = computed(() =>
+  commonCards.value.filter((card) => card.colors.length === 1 && card.colors[0] === 'U'),
+)
+
+const selectedBlackCard = ref<Card>()
+const blackCommons = computed(() =>
+  commonCards.value.filter((card) => card.colors.length === 1 && card.colors[0] === 'B'),
+)
+
+const selectedRedCard = ref<Card>()
+const redCommons = computed(() =>
+  commonCards.value.filter((card) => card.colors.length === 1 && card.colors[0] === 'R'),
+)
+
+const selectedGreenCard = ref<Card>()
+const greenCommons = computed(() =>
+  commonCards.value.filter((card) => card.colors.length === 1 && card.colors[0] === 'G'),
 )
 </script>
 
@@ -20,10 +41,34 @@ const whiteCards = computed(() =>
       <h1>SOS p0p1</h1>
       <form>
         <CardChoice
-          :cards="whiteCards"
+          :cards="whiteCommons"
           :selected-card="selectedWhiteCard"
           label="White Common"
           @select-card="(card) => (selectedWhiteCard = card)"
+        />
+        <CardChoice
+          :cards="blueCommons"
+          :selected-card="selectedBlueCard"
+          label="Blue Common"
+          @select-card="(card) => (selectedBlueCard = card)"
+        />
+        <CardChoice
+          :cards="blackCommons"
+          :selected-card="selectedBlackCard"
+          label="Black Common"
+          @select-card="(card) => (selectedBlackCard = card)"
+        />
+        <CardChoice
+          :cards="redCommons"
+          :selected-card="selectedRedCard"
+          label="Red Common"
+          @select-card="(card) => (selectedRedCard = card)"
+        />
+        <CardChoice
+          :cards="greenCommons"
+          :selected-card="selectedGreenCard"
+          label="Green Common"
+          @select-card="(card) => (selectedGreenCard = card)"
         />
       </form>
     </UContainer>
