5ee4b6d1283f22ddce51768cfa3dfc867275dfbe
Sean Li <9913851+saifahn@users.noreply.github.com>
2026-04-18 22:57:36 +0900

    fix(deps): actually cleanup use of unused dependency

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,9 @@ import ui from '@nuxt/ui/vue-plugin'
 import { createApp } from 'vue'
 import App from './App.vue'
 import './style.css'
-import { firebaseApp } from './firebase'
-import { VueFire } from 'vuefire'
 
 const app = createApp(App)
 
 app.use(ui)
-
-app.use(VueFire, {
-  firebaseApp,
-})
 
 app.mount('#app')
