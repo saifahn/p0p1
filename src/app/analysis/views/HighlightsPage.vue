8f8644b545fd31ebd74968af2f03f037e3736199
Sean Li <9913851+saifahn@users.noreply.github.com>
2026-05-21 09:27:04 +0900

    wip: add highlights page

---
+++ src/app/analysis/views/HighlightsPage.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <UContainer as="main" class="p-2 pb-12 max-w-5xl">
+    <h1 class="text-2xl font-bold py-4">Highlights</h1>
+  </UContainer>
+</template>
